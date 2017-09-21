8e00cbbfceae9f3247a97461d64dfc3d0b6fbb9c
tonichaz <toni.chaz@intelygenz.com>
2017-09-21 17:00:58 +0200

    Optimize code

--- solosisnet-model/run.py
+++ solosisnet-model/run.py
@@ -7,43 +7,45 @@ from keras.models import load_model
 from flask import request, url_for
 from flask_api import FlaskAPI, status, exceptions
 
-# Start FlaskAPI
-app = FlaskAPI(__name__)
 
-@app.route("/", methods=['GET','POST'])
-def pokemons_combat():
-    """
-    Create pokemons combat.
-    """
-    if request.method == 'POST':
+if __name__ == '__main__':
 
-        # Load model...
-        solosisnet = load_model("solosisnet.h5")
+    # Load model...
+    solosisnet = load_model("solosisnet.h5")
 
-        stats = pd.read_csv('data/formated_stats.csv', index_col='#')
+    stats = pd.read_csv('data/formated_stats.csv', index_col='#')
 
-        # Set combat to predict:
-        pok_A_id = request.data.get('pokA', '')
-        pok_B_id = request.data.get('pokB', '')
+    # Start FlaskAPI
+    app = FlaskAPI(__name__)
 
-        # Format net input
-        stats_A = stats[stats.index == pok_A_id].values[0].tolist()
-        stats_B = stats[stats.index == pok_B_id].values[0].tolist()
-        net_input = np.array([stats_A + stats_B])
+    @app.route("/", methods=['GET','POST'])
+    def pokemons_combat():
+        """
+        Create pokemons combat.
+        """
+        if request.method == 'POST':
 
-        # Make a prediction...
-        prediction = solosisnet.predict(net_input)
+            # Set combat to predict:
+            pok_A_id = request.data.get('pokA', '')
+            pok_B_id = request.data.get('pokB', '')
 
-        if prediction < 0.5:
-            # Send pokemon_A
-            return [{'winner': 'A'}], status.HTTP_200_OK
-        else:
-            # Send pokemon_B
-            return [{'winner': 'B'}], status.HTTP_200_OK
+            # Format net input
+            stats_A = stats[stats.index == pok_A_id].values[0].tolist()
+            stats_B = stats[stats.index == pok_B_id].values[0].tolist()
+            net_input = np.array([stats_A + stats_B])
 
-    # request.method == 'NOT_SUPPORTED'
-    return [status.HTTP_415_UNSUPPORTED_MEDIA_TYPE]
+            # Make a prediction...
+            prediction = solosisnet.predict(net_input)
 
-if __name__ == '__main__':    
+            if prediction < 0.5:
+                # Send pokemon_A
+                return [{'winner': 'A'}], status.HTTP_200_OK
+            else:
+                # Send pokemon_B
+                return [{'winner': 'B'}], status.HTTP_200_OK
+
+        # request.method == 'NOT_SUPPORTED'
+        return [status.HTTP_415_UNSUPPORTED_MEDIA_TYPE]
+
     # Debug true
-    app.run(debug=True)
+    app.run()
